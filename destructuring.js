369a0b890d624c5632c6b57454dcd830157b17ef
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2024-08-12 09:52:55 +0600

    'added'

---
+++ destructuring.js
@@ -0,0 +1,9 @@
+// 1. array destructuring
+
+const number = [22, 33, 44];
+
+const [x, y, z] = number
+console.log(x, y, z);
+
+// 2. objects destructuring
+const {name, age }= {name: 'alu', age: 14}
