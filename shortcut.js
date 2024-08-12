9b69a8ab6dba34d4655e4fe62151f70f3eb3126a
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2024-08-12 10:35:13 +0600

    'added'

---
+++ shortcut.js
@@ -0,0 +1,17 @@
+// truthy = 'aaa' , 5 , true , {}, []
+
+// falsy = '' , 0, false, null , undefine
+
+let myVar = 5;
+if (myVar) {
+    myVar = myVar * 100;
+}
+else {
+    myVar = 0
+}
+let money = 80 ;
+let food ;
+// ternary
+
+let food1 = money > 100? 'birani' : ' cha';
+console.log(food1);
